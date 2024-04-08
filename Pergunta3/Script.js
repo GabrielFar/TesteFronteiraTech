fc7d2ceb0e0fa28b986ff73a984bbe936558e529
unknown <gabriel.farina2358@gmail.com>
2024-04-08 20:03:10 -0300

    add css

---
+++ Pergunta3/Script.js
@@ -0,0 +1,13 @@
+var valor1
+var valor2
+var operacao
+var tela = document.getElementById("idInput")
+console.log(tela)
+
+function igual(resultado) {
+    tela.value = resultado
+};
+
+function num(num){
+    tela.value = tela.value + num
+};
